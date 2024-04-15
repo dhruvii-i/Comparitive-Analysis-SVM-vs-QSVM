6a6196fe187311daabdefe632b94202fa965587c
Dhruvi Ranwala <144709918+dhruvii-i@users.noreply.github.com>
2024-04-15 12:14:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pickle
+
+# Load the model
+with open('YourModel.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Define your Streamlit app here
+def main():
+    st.title('Your Streamlit App')
+    st.write('This is a simple Streamlit app.')
+
+if __name__ == '__main__':
+    main()
